422a4454b30741a92c6260e7084845af1a592ad7
Abhijeet Soni <abhisonijeet123@gmail.com>
2025-07-22 21:52:12 +0530

    serverjs

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(cors(
     {
-        origin: ['http://localhost:5173','https://clip-nest-sigma.vercel.app/'],
+        origin: ['http://localhost:5173','https://clip-nest-sigma.vercel.app/','https://clipnest-4m02.onrender.com'],
         credentials: true,
     }
 ))
